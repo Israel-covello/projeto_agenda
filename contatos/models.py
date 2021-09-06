da3d3b103e8ed507e8e48783b5f3ad2b536199de
Israel Covello Jr <israel123.covello@gmail.com>
2021-09-06 15:44:21 -0300

    Criação da base de dados

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=155)
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=55)
+    sobrenome = models.CharField(max_length=55, blank=True) # Campo opcional
+    telefone = models.CharField(max_length=55)
+    email = models.CharField(max_length=155, blank=True) # Campo opcional
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(max_length=5000, blank=True)
+    Categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
