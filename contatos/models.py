ea7be3c1b2260e751d250c7bec305e68205558f7
Israel Covello Jr <israel123.covello@gmail.com>
2021-09-06 16:14:04 -0300

    Modificando area admin do Django

--- contatos/models.py
+++ contatos/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=155)
 
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=55)
     sobrenome = models.CharField(max_length=55, blank=True) # Campo opcional
@@ -11,4 +14,7 @@ class Contato(models.Model):
     email = models.CharField(max_length=155, blank=True) # Campo opcional
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(max_length=5000, blank=True)
-    Categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
