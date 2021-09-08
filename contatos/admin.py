e5add60a2e14d75eee0f0ebbcf700a638c3989af
Israel Covello Jr <israel123.covello@gmail.com>
2021-09-08 13:29:56 -0300

    Corrigindo erro 404 e adicionando campo mostrar contatos na área admin

--- contatos/admin.py
+++ contatos/admin.py
@@ -5,7 +5,7 @@ from .models import Categoria, Contato
 class ContatoAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'nome', 'sobrenome', 'telefone', 'email',
-        'data_criacao', 'categoria'
+        'data_criacao', 'categoria', 'mostrar'
         )
     list_display_links = (
         'id', 'nome', 'sobrenome'
@@ -13,6 +13,9 @@ class ContatoAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = (
         'nome', 'sobrenome', 'telefone', 'categoria'
+    )
+    list_editable = (
+        'telefone', 'mostrar'
     )
 
 
